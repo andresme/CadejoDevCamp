c5ed0c7689dd5365172ab2001f4dc2b5293533d7
andresme <andres.me@gmail.com>
2013-04-06 16:30:32 -0600

    API 10

--- cadejo-android/src/com/devcamp/cadejo/ScoreActivity.java
+++ cadejo-android/src/com/devcamp/cadejo/ScoreActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,7 +16,7 @@ public class ScoreActivity extends Activity implements IConstants{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.score);
 		setButtonEvents();
 		showScore();
